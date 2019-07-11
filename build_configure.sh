0753dce7eff80127837c617dc27f284f28d8e12c
simonqin09 <simonqin09@gmail.com>
2019-07-11 14:32:20 +0800

    Update build_configure.sh

--- build_configure.sh
+++ build_configure.sh
@@ -3,7 +3,7 @@
 # configure bash for qt5.12.3
 ./configure -v \
     -opensource -confirm-license \
-    -extprefix /usr/local/Qt-5.12.3 \
+    -prefix /usr/local/Qt-5.12.3 \
     -device linux-imx6-g++ \
     -device-option CROSS_COMPILE=${CROSS_COMPILE} \
     -sysroot ${SDKTARGETSYSROOT} \
